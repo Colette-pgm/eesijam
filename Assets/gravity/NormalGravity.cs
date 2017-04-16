5790554615b39f5e31601efffec8abd74fde7095
Unknown <alexis.martial16@gmail.com>
2017-04-17 01:40:03 +0200

    jvvhjsce

--- Assets/gravity/NormalGravity.cs
+++ Assets/gravity/NormalGravity.cs
@@ -11,7 +11,7 @@ public class NormalGravity : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		m_rb.AddForce (Vector2.down * gravityScale);
 	}
 }
